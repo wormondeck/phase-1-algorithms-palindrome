1c245df0a531af5cea753c9fbfef385bdae2f059
Ronney Arias <ronneyarias@gmail.com>
2023-11-09 09:59:26 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  // Initialize pointers
+  let start = 0;
+  let end = str.length - 1;
+
+  // Compare characters at the pointers
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false; 
+    }
+    start++;
+    end--;
+  }
+
+  return true; // Palindrome
 }
 
 /* 
   Add your pseudocode here
+
+cleanedStr = Removes non-alphanumeric characters from the string
+  lowercaseStr = Converts the string to lowercase
+  start = 0 // Initialize start pointer
+  end = LENGTH(lowercaseStr) - 1 // Initialize the end pointer
+
+  WHILE start < end // Loop through the string until the pointers meet
+    IF lowercaseStr[start] != lowercaseStr[end] // Check if the characters at the pointers are not equal
+      RETURN false // string is not a palindrome
+
+    INCREMENT start // Move the start pointer forward
+    DECREMENT end // Move the end pointer backward
+
+  RETURN true // The string is a palindrome
 */
+
+
+
+
 
 /*
   Add written explanation of your solution here
